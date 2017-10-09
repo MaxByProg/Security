08099cefbdbcfb5939e7b4dc088504c419148bcd
mbikov <mbikov@tsconsulting.ru>
2017-10-09 18:57:01 +0300

    added script for sql db

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,46 +1,22 @@
-CREATE DATABASE  IF NOT EXISTS `accounts`;
-USE `accounts`;
---
--- Table structure for table `role`
---
+CREATE TABLE role (
+  id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+  name VARCHAR2(45) DEFAULT NULL,
+  PRIMARY KEY (id));
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE  TABLE users (
+  id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
+  username VARCHAR2(255) DEFAULT NULL,
+  password VARCHAR2(255) DEFAULT NULL,
+  PRIMARY KEY (id));
 
---
--- Dumping data for table `role`
---
+CREATE TABLE user_role (
+  user_id NUMBER NOT NULL,
+  role_id NUMBER NOT NULL,
+  PRIMARY KEY (user_id, role_id)
+);
 
-LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'ROLE_USER');
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `user_role`
---
-
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role` (
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `fk_user_role_roleid_idx` (`role_id`),
-  CONSTRAINT `fk_user_role_roleid` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user_role_userid` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE INDEX fk_user_role_roleid_idx ON USER_ROLE(role_id);
+ALTER TABLE user_role
+  ADD CONSTRAINT fk_user_role_roleid FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE;
+ALTER TABLE user_role
+  ADD	CONSTRAINT fk_user_role_userid FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
